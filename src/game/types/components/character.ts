b178d094cf18aad7654bdf40bc38fca5bfd36b92
Bbanez <banezprod@gmail.com>
2021-05-05 22:56:46 +0200

    Add char classes and subclasses

--- src/game/types/components/character.ts
+++ src/game/types/components/character.ts
@@ -1,15 +1,52 @@
 import type { PositionVector } from '../vector';
 import type { BoxDimensions } from '../bounding-box';
 
+// eslint-disable-next-line no-shadow
 export enum CharacterRace {
-  HUMAN =  'HUMAN',
-  ELF = 'ELF',
-  DWARF = 'DWARF',
-
+  HUMAN = 'HUMAN',
+  ANKY = 'ANKY',
 }
-export type CharacterType = 'archer' | 'assassin' | 'mage' | 'berserk' | ''
+// eslint-disable-next-line no-shadow
+export enum CharacterClass {
+  RANGER = 'RANGER', // HUMAN, ANKY
+  MEMIST = 'MEMIST', // ANKY
+  GLADIATOR = 'GLADIATOR', // HUMAN
+  PROTECTOR = 'PROTECTOR', // HUMAN, ANKY
+  SHAMAN = 'SHAMAN', // HUMAN, ANKY
+}
+// eslint-disable-next-line no-shadow
+export enum CharacterSubclass {
+  /**
+   * RANGER
+   */
+  ARCHER = 'ARCHER', // DPS
+  MARKSMEN = 'MARKSMAN', // BUFF+DPS
+  /**
+   * MEMIST
+   */
+  ENHANCER = 'ENHANCER', // BUFF
+  MEISTER = 'MEISTER', // DPS
+  /**
+   * GLADIATOR
+   */
+  BERSERK = 'BERSERK', // DPS+AOE
+  DUELIST = 'DUELIST', // DPS+SINGLE
+  /**
+   * PROTECTOR
+   */
+  GUARDIAN = 'GUARDIAN', // TANK
+  PALADIN = 'PALADIN', // TANK+HEAL
+  /**
+   * SHAMAN
+   */
+  ANKYMAN = 'ANKYMAN', // HEAL+DEFENSE
+  CASTER = 'CASTER', // HEAL+ATTACK
+}
 export interface CharacterStats {
-  type:
+  class: {
+    type: CharacterClass;
+    subclass?: CharacterSubclass;
+  };
   strength: number;
   agility: number;
   wisdom: number;
